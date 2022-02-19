d8d2ba7b3b6a9a83878934231e3c60327087ef13
alejovillores <alejovillores@gmail.com>
2022-02-19 12:13:44 -0300

    bash scripping

---
+++ command.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# para hacer el archivo ejecutable ejecuto luego en la terminal chmod a+rx command.sh
+# esto es un comentario
+
+cd /home/alejo/Documentos #primero voy hacia la carpeta documento
+touch text.txt # creo un archivo de texto
+chmod a+rx command.sh
+
---
+++ test_2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# para hacer el archivo ejecutable ejecuto luego en la terminal chmod a+rx command.sh
+# esto es un comentario
+
+cd /home/alejo/Documentos #primero voy hacia la carpeta documento
+touch text_2.txt # creo un archivo de texto
+chmod a+rx test_2.sh
+
+
